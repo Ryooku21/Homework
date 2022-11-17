f8a4e52f474ba3d35c599fb3b5e55b7d14b47ceb
Maksim <maksim_kabanov_83@mail.ru>
2022-11-17 13:06:23 +0300

    solution with negative check

---
+++ Task 08/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число");
+int singleDigit = Convert.ToInt32(Console.ReadLine());
+int step = 1;
+
+while (step <= singleDigit) 
+{
+    if (step % 2 == 0) 
+    {
+        Console.WriteLine(step);
+    } 
+        step++;
+}
+
+if (singleDigit < 0) {
+    while (step >= singleDigit) 
+{
+    if (step % 2 == 0) 
+    {
+        Console.WriteLine(step);
+    } 
+        step--;
+}
+}
