54bf99e9c2c1f8dc1cca329094c433ea29165d77
Maksim <maksim_kabanov_83@mail.ru>
2022-11-17 12:49:11 +0300

    best  solution

--- Task 04/Program.cs
+++ Task 04/Program.cs
@@ -10,16 +10,19 @@ int firstDigit = Convert.ToInt32(Console.ReadLine());
 int secondDigit = Convert.ToInt32(Console.ReadLine());
 int thirdDigit = Convert.ToInt32(Console.ReadLine());
 
-int showmax = firstDigit;
-if (showmax > secondDigit && showmax > thirdDigit) {
-    Console.WriteLine($"Максимальное число равно {showmax}");
-} else
-if (showmax < secondDigit && secondDigit > thirdDigit)
+if (firstDigit > secondDigit) {
+    if (firstDigit > thirdDigit) 
 {
-showmax = secondDigit;
-Console.WriteLine($"Максимальное число равно {showmax}");
-} else
-if (showmax < secondDigit && secondDigit < thirdDigit) {
-    showmax = thirdDigit;
-Console.WriteLine($"Максимальное число равно {showmax}");
-} 
+        Console.WriteLine($"Максимальное число равно {firstDigit}");
+}
+    else {
+        Console.WriteLine($"Максимальное число равно {thirdDigit}");
+ }}
+    else { 
+        if (secondDigit > thirdDigit) {
+
+            Console.WriteLine($"Максимальное число равно {secondDigit}");
+}
+    else {
+            Console.WriteLine($"Максимальное число равно {thirdDigit}");
+}}
