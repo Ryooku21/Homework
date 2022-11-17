ab9d8245a962632f360069b6a7de5cfd3f3cd1a1
Maksim <maksim_kabanov_83@mail.ru>
2022-11-17 12:32:14 +0300

    solved

--- Task 06/Program.cs
+++ Task 06/Program.cs
@@ -5,3 +5,14 @@
 // -3 -> нет
 // 7 -> нет
 
+Console.WriteLine("Введите целое число");
+int singleDigit = Convert.ToInt32(Console.ReadLine());
+
+if (singleDigit == 0) {
+    Console.WriteLine("Введите число отличное от 0");
+} else
+if (singleDigit % 2 == 0) {
+    Console.WriteLine("Введенное число является четным");
+} else {
+    Console.WriteLine("Введенное число является нечетным");
+}
