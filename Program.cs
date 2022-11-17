2ac5f32c69a506f56a088b433bc6d57edd28d1dd
Maksim <maksim_kabanov_83@mail.ru>
2022-11-17 10:17:28 +0300

    added solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите два целых числа");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+if (firstDigit == secondDigit) {
+    Console.WriteLine($"Оба числа равны {firstDigit} = {secondDigit}");
+}
+    else
+    if (firstDigit > secondDigit) {
+        Console.WriteLine($"Первое число {firstDigit} больше, чем второе {secondDigit}");
+}   else
+{
+        Console.WriteLine($"Второе число {secondDigit} больше, чем первое {firstDigit}");
+}
